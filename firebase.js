e72e2d25bfd562f7ec1871ad4b5c28593ec6cd9d
devorxfvckers <alexjrsullera9@gmail.com>
2026-06-21 10:57:07 -0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,17 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// 🔥 YOUR REAL FIREBASE CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyCE1Jk_rZPDnYe6VqdP5iG6je5_U2UI5Kk",
   authDomain: "my-dashbord-app.firebaseapp.com",
   projectId: "my-dashbord-app",
-  storageBucket: "my-dashbord-app.firebasestorage.app",
+  storageBucket: "my-dashbord-app.appspot.com",
   messagingSenderId: "273313686403",
   appId: "1:273313686403:web:0210e790828a98954accb6"
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
